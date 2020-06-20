0dc1c75adce38c1abee18683e10bdbd072bcfcd4
MuYao <shuo.zhang@scasia-core.com>
2020-06-20 18:15:27 +0800

    增加注释

--- src/test/java/com/wenruo/OrderTest.java
+++ src/test/java/com/wenruo/OrderTest.java
@@ -20,6 +20,7 @@ public class OrderTest {
     @Autowired
     private static OrderMapper orderMapper;
 
+    /* 指定年份 */
     private static final String YEAR = "2019";
 
     @Test
@@ -29,9 +30,8 @@ public class OrderTest {
         List<Order> orderMapperList = orderMapper.selectOrder();
         /* 获取2019年的单据 */
         orderMapperList = orderMapperList.stream()
-                .peek(order -> {
-                    order.getLocalDateTime().substring(0, 4).equals(YEAR);
-                }).collect(Collectors.toList());
+                .peek(order -> order.getLocalDateTime().substring(0, 4).equals(YEAR))
+                .collect(Collectors.toList());
         /* 按支行号分组 */
         Map<String, List<Order>> collect = orderMapperList.stream()
                 .collect(Collectors.groupingBy(Order::getAreaNo));
@@ -49,12 +49,16 @@ public class OrderTest {
             Map<String, List<Order>> mouthMap = collect.get(areaNo).stream()
                     .collect(Collectors.groupingBy(order -> order.getLocalDateTime().substring(0, 6)));
 
+            /* 获取按小时分类的数据，并压入输出的list中 */
             outOrderList.addAll(getDate(hourMap));
 
+            /* 获取按天分类的数据，并压入输出的list中 */
             outOrderList.addAll(getDate(dayMap));
 
+            /* 获取按月分类的数据，并压入输出的list中 */
             outOrderList.addAll(getDate(mouthMap));
 
+            /* 输出2019按月、日、小时的数据 */
             System.out.println(outOrderList);
         });
     }
@@ -64,23 +68,30 @@ public class OrderTest {
 
         map.keySet().forEach(localDateTime -> {
             Order orderHour = new Order();
+
             /* 总交易量 */
             BigDecimal countDeal = map.get(localDateTime).stream()
                     .map(Order::getCountDeal)
                     .reduce(BigDecimal.ZERO,BigDecimal::add);
+
             /* 最大交易量 */
             BigDecimal maxDeal = map.get(localDateTime).stream()
                     .map(Order::getCountDeal).max(BigDecimal::compareTo)
                     .get();
+
             /* 总平均交易金额 */
             BigDecimal avDelMoney = map.get(localDateTime).stream()
                     .map(order -> order.getAvgDelMoney().multiply(order.getCountDeal()))
                     .reduce(BigDecimal.ZERO, BigDecimal::add).divide(countDeal);
+
+            /* 将数据存进实体 */
             orderHour.setLocalDateTime(localDateTime.substring(0,10))
                     .setLogo(1)
                     .setCountDeal(countDeal)
                     .setAvgDelMoney(avDelMoney)
                     .setMaxDeal(maxDeal);
+
+            /* 将每条数据存入list输出 */
             orderList.add(orderHour);
         });
 
